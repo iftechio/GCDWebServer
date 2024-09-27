69030cde28c4e5fa7b285992d2690bce216ab94f
Kael <kael.zs17@gmail.com>
2024-09-27 12:41:38 +0800

    feat: from official release 3.5.4

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GCDWebServer",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "GCDWebServer",
+            type: .static,
+            targets: ["GCDWebServer"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "GCDWebServer",
+            path: "Sources/GCDWebServer",
+            cxxSettings: [
+                .headerSearchPath("Core"),
+            ],
+            linkerSettings: [
+                .linkedLibrary("z"),
+                .linkedFramework("CoreServices"),
+                .linkedFramework("CFNetwork"),
+            ]
+        ),
+
+    ]
+)
